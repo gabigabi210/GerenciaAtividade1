4b0336b39b1dc908962946c989714e5df76eda3c
Gabi <gabimelo640@gmail.com>
2025-10-05 19:11:43 -0300

    adição da explicação

--- src/Explicacao.java
+++ src/Explicacao.java
@@ -1,16 +1,15 @@
 public class Explicacao {
     Pedido pedido;
-    public String informacoesValorTotal(double frete, double imposto, double desconto, double total) {
+    public String informacoesValorTotal(double frete, double imposto, double total) {
         String relatorio = """
 =================================
 |   Relatório do valor pago    |
 =================================
 | Frete: %.2f                  |
 | Impostos: %.2f               |
-| Descontos: %.2f              |
 | Total: %.2f                  |
 =================================
 """;
-        return String.format(relatorio, frete, imposto, desconto, total);
+        return String.format(relatorio, frete, imposto, total);
     }
 }
