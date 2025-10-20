57742ef59eb82fd01bcde32b4da4d88dae3fee5d
Glauco <glauco0909@gmail.com>
2025-10-20 10:15:55 -0300

    para reset

--- src/LogicaPagamento/PagamentoMaisDe12Vezes.java
+++ src/LogicaPagamento/PagamentoMaisDe12Vezes.java
@@ -3,7 +3,7 @@ package LogicaPagamento;
 public class PagamentoMaisDe12Vezes implements TipoPagamento{
     @Override
     public double realizarPagamento(double valor) {
-        return valor * 1.2;
+        return valor * 2;
     }
 
 }
